3dfe12223ae6ae821ed8ac90d9eeca4694d3aad6
JesperSundstrom <jesper.sundstrom@hotmail.com>
2016-04-18 11:47:57 +0200

    Synki

--- WP/wp-content/themes/start/index.php
+++ WP/wp-content/themes/start/index.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="wrapper">
 		<main id="main" class="site-main col-md-8" role="main">
 
 		<?php if ( have_posts() ) : ?>
@@ -42,5 +42,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
